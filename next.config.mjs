a58705167fb8eae32aca7d580e68020a959c5ad6
Shunsuke Hochu <124444636+hochu-shunsuke@users.noreply.github.com>
2025-06-15 17:52:30 +0900

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  distDir: 'dist',
+  images: {
+    unoptimized: true
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/kenkyu-kaihatu-literacy-js' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/kenkyu-kaihatu-literacy-js/' : '',
+};
 
 export default nextConfig;
